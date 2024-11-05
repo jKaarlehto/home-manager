93e51d819d77dce69c0ffaf4d0ed199bb412d469
Juhana Kaarlehto <60972018+jKaarlehto@users.noreply.github.com>
2024-11-05 14:37:56 +0200

    add follow for home-manager and nixvim

--- flake.nix
+++ flake.nix
@@ -3,7 +3,9 @@
   inputs = {
     nixpkgs.url = "flake:nixpkgs/nixpkgs-unstable";
     home-manager.url = "flake:home-manager";
+    home-manager.follow = "nixpkgs"
     nixvim.url = "github:nix-community/nixvim";
+    nixvim.follow = "nixpkgs"
   };
   outputs = inputs:
     let
