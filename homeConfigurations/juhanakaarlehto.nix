6169824e7e456cf71511e1105367064f0a434e75
Juhana Kaarlehto <60972018+jKaarlehto@users.noreply.github.com>
2024-11-06 15:44:46 +0200

    Update juhanakaarlehto.nix

--- homeConfigurations/juhanakaarlehto.nix
+++ homeConfigurations/juhanakaarlehto.nix
@@ -17,15 +17,16 @@ let
         sessionVariables = {
           SHELL = "${pkgs.zsh}/bin/zsh";
         };
-        
-	# Install MacOS applications to the user environment if the targetPlatform is Darwin
-	file."Applications/home-manager".source = let
-	apps = pkgs.buildEnv {
-	  name = "home-manager-applications";
-	  paths = config.home.packages;
-	  pathsToLink = "/Applications";
-	};
-	  in mkIf pkgs.stdenv.targetPlatform.isDarwin "${apps}/Applications";
+
+	activation = {
+    	  aliasApplications = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      	  app_folder=$(echo ~/Applications);
+     	  for app in $(find "$genProfilePath/home-path/Applications" -type l); do
+            $DRY_RUN_CMD rm -f $app_folder/$(basename $app)
+            $DRY_RUN_CMD osascript -e "tell app \"Finder\"" -e "make new alias file at POSIX file \"$app_folder\" to POSIX file \"$app\"" -e "set name of result to \"$(basename $app)\"" -e "end tell"
+	  done
+ 	'';
+ 	};
       };
 
       nixpkgs = {
