88cdcca282ba3530d95aa7f1403c67ec261b4d63
Juhana Kaarlehto <juhana.kaa@gmail.com>
2024-11-09 13:59:04 +0200

    fix newline

--- homeConfigurations/juhanakaarlehto.nix
+++ homeConfigurations/juhanakaarlehto.nix
@@ -53,8 +53,9 @@ let
           defaultKeymap = "viins";
           enable = true;
           initExtra = "# Nix
-if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' fi \n
-# End Nix[ -z \"\$TMUX\" ] && { tmux attach || exec tmux new-session; } && exit";
+if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' fi
+# End Nix
+[ -z \"\$TMUX\" ] && { tmux attach || exec tmux new-session; } && exit";
         };
       };
     };
