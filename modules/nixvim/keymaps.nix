f8e821741a927c8ad235ce6ce66ed6ff854cb5bb
Juhana Kaarlehto <juhana.kaa@gmail.com>
2024-11-16 17:27:57 +0200

    visual mode keymaps

--- modules/nixvim/keymaps.nix
+++ modules/nixvim/keymaps.nix
@@ -1,13 +1,44 @@
 { config, pkgs, ... }:
 {
-
-	programs.nixvim = {
-		
+		programs.nixvim = {
 		globals.mapleader = " ";
 		keymaps = [
-
-
+				{
+						mode = "n";
+						key = "<leader>e";
+						action = ":Vex<CR>";			
+						options.silent = true;
+				}
+				{
+						key = "<Space>";
+						action = "<Nop>";
+				}
+				#Move visual block with <S-[h|j|k|l]>
+				{
+						mode = "v";
+						key = "<S-j>";
+						action = ":m '>+1<CR>gv=gv";
+						options.silent = true;
+				}
+				{
+						mode = "v";
+						key = "<S-k>";
+						action = ":m '>-2<CR>gv=gv";
+						options.silent = true;
+				}
+				{
+						mode = "v";
+						key = "<S-h>";
+						action = "<gv";
+						options.silent = true;
+				}
+				{
+						mode = "v";
+						key = "<S-l>";
+						action = ">gv";
+						options.silent = true;
+				}
+				
 		];
-
 	};
 }
